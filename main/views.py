28226937c1c9df7dd16489652851ddf09ed15868
sar1onqwe <sar1onqwe@gmail.com>
2024-09-28 21:53:08 +0800

    About Page

--- main/views.py
+++ main/views.py
@@ -2,7 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина'
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About for me')
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина',
+        'about': 'Мы лучшие в своем деле!)'
+    }
+    return render(request, 'main/about.html', context)
