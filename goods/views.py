85338268302c9d138f08ea5d73a744a2914d79e3
sar1onqwe <sar1onqwe@gmail.com>
2024-10-02 20:56:24 +0800

    Paginator

--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.core.paginator import Paginator
 
 from goods.models import Products
 
-def catalog(request):
+def catalog(request, category_slug, page=1):
 
-    goods = Products.objects.all()
+    if category_slug == 'vse-tovary':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
+
 
     context = {
-        'goods': goods
+        'goods': current_page,
+        'slug_url': category_slug
     }
     return render(request, 'goods/catalog.html', context)
 
