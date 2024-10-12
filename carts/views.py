34f5f1185dc6ee65a3907bfd5ed2ec25d26be777
sar1onqwe <sar1onqwe@gmail.com>
2024-10-12 16:02:18 +0800

    Cart remove controller ready

--- carts/views.py
+++ carts/views.py
@@ -6,8 +6,12 @@ from goods.models import Products
 def cart_change(request, product_slug):
     ...
 
-def cart_remove(request, product_slug):
-    ...
+def cart_remove(request, cart_id):
+    
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    
+    return redirect(request.META['HTTP_REFERER'])
 
 def cart_add(request, product_slug):
     
