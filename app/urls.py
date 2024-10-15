3dcd46b500ea7e0f08657bfe407a72d70648788b
sar1onqwe <sar1onqwe@gmail.com>
2024-10-15 21:28:07 +0800

    Orders templates ready

--- app/urls.py
+++ app/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('user/', include('users.urls', namespace='user')),
     path('cart/', include('carts.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]
 
 if settings.DEBUG:
